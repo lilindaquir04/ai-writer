6d33439c63ab299f78b7ad83179f424368a63c60
Marat Bulatov <bulatoffmaratka@yandex.ru>
2025-09-28 23:42:39 +0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: "google/gemma-3-12b-it:free",
+        model: "meta-llama/llama-3-8b-instruct:free",
         messages: [
           {
             role: "system",
